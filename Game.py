e3616c467526f5ad7aa2876485065d43b9efa274
jethet <henriette.hettinga@gmail.com>
2018-11-28 17:30:01 +0100

    Create Game Challenge

---
+++ Game.py
@@ -0,0 +1,6 @@
+#Import random module to use randint (creates random integer) to act as dice
+from random import randint
+
+#imitate roll of dice but only generating 0, 1 or 2
+roll = randint(0, 2)
+print(roll)
