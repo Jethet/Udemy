2a6fda4ef13a47d0287f311b3c049805f3e8f0f4
jethet <henriette.hettinga@gmail.com>
2018-11-29 13:42:28 +0100

    First Function Practice exercise

---
+++ ExerCompare.py
@@ -0,0 +1,15 @@
+#Compare two values: if even, return smallest; if odd, return biggest.
+
+def compare(x, y):
+    if True:
+        x % 2 == 0 and y % 2 == 0
+        print(min(x, y))
+    elif False:
+        print(max(x, y))
+    else:
+        print("Enter integers only.")
+
+compare(2, 6)
+compare(7, 9)
+compare(7, 7)
+compare(7, 7.5)
