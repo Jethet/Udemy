461af30d1aada0d3180625229420f49dd6ddfbdf
jethet <henriette.hettinga@gmail.com>
2018-11-29 12:37:00 +0100

    Final version

--- Game.py
+++ Game.py
@@ -8,14 +8,18 @@ print("Welcome to the game. You can enter a number between 1 and 100.\
 You will get one of these messages: 1) YOU WON! 2) WARM! 2) COLD!\
 Message 2 means you picked a number within 10 values below or above\
 the number you must guess. Message 3 means your guess is further away\
-from the solution. Have fun!")
+from the solution. You can stop anytime by typing 0. Have fun!")
 
 while True:
     try:
 #the player is asked for a number
         guess = int(input("Please pick a number. "))
+#the user can stop any time:
+        if guess == 0:
+            print("Thank you for playing!")
+            break
 #the number must be between 1 and 100
-        if guess < 1 or > 100:
+        elif guess < 1 or guess > 100:
             print("Please enter a number between 1 and 100.")
             continue
 #if user guess is equal to random, print YOU WON!
@@ -30,10 +34,6 @@ while True:
 #if user guess is bigger than random plus 10, print COLD!
         elif guess < (guess_int - 10) or guess > (guess_int + 10):
             print("COLD!")
-#the user can stop any time:
-        elif guess == 'Stop':
-            print("Thank you for playing!")
-            break
 #the input has to be an integer
     except ValueError:
         print("Please only enter one number as correct input.")
