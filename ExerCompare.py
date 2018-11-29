7072fd3ba8002f3ec38e5c04e7700a4e14c59a46
jethet <henriette.hettinga@gmail.com>
2018-11-29 13:51:48 +0100

    Check NameError

--- ExerCompare.py
+++ ExerCompare.py
@@ -1,15 +1,17 @@
 #Compare two values: if even, return smallest; if odd, return biggest.
 
 def compare(x, y):
-    if True:
-        x % 2 == 0 and y % 2 == 0
-        print(min(x, y))
-    elif False:
-        print(max(x, y))
-    else:
-        print("Enter integers only.")
+    try:
+        if True:
+            x % 2 == 0 and y % 2 == 0
+            print(min(x, y))
+        elif False:
+            print(max(x, y))
+#the exception is meant to prevent raising an error in case of characters
+    except NameError:
+        print("Please enter numbers only.")
 
 compare(2, 6)
 compare(7, 9)
 compare(7, 7)
-compare(7, 7.5)
+compare(7, a)    #this raises a NameError so the exception does not work
