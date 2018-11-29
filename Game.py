90d13f5a1c3992ef9a6f41e25232d30c2003be8d
jethet <henriette.hettinga@gmail.com>
2018-11-29 12:31:49 +0100

    Add option to stop

--- Game.py
+++ Game.py
@@ -12,23 +12,29 @@ from the solution. Have fun!")
 
 while True:
     try:
-        #the player is asked for a number
+#the player is asked for a number
         guess = int(input("Please pick a number. "))
-        #if user guess is equal to random, print YOU WON!
-        if guess == guess_int:
+#the number must be between 1 and 100
+        if guess < 1 or > 100:
+            print("Please enter a number between 1 and 100.")
+            continue
+#if user guess is equal to random, print YOU WON!
+        elif guess == guess_int:
             print("YOU WON!")
             break
-
-        #if user guess is bigger than the random minus 10, print WARM!
-        #if user guess is smaller than random plus 10, print WARM!
+#if user guess is bigger than the random minus 10, print WARM!
+#if user guess is smaller than random plus 10, print WARM!
         elif guess >= (guess_int - 10) and guess <= (guess_int + 10):
             print("WARM!")
-
-        #if user guess is smaller than random minus 10, print COLD!
-        #if user guess is bigger than random plus 10, print COLD!
+#if user guess is smaller than random minus 10, print COLD!
+#if user guess is bigger than random plus 10, print COLD!
         elif guess < (guess_int - 10) or guess > (guess_int + 10):
             print("COLD!")
-
+#the user can stop any time:
+        elif guess == 'Stop':
+            print("Thank you for playing!")
+            break
+#the input has to be an integer
     except ValueError:
         print("Please only enter one number as correct input.")
         continue
