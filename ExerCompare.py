20ae51370a82698e87eeb5ae0dce0f377b3dfe43
jethet <henriette.hettinga@gmail.com>
2018-11-30 14:36:25 +0100

    Change comments

--- ExerCompare.py
+++ ExerCompare.py
@@ -1,17 +1,16 @@
-#Compare two values: if even, return smallest; if odd, return biggest.
+#Compare two values: if two values are even, return smallest; if odd, return biggest.
 
 def compare(x, y):
     try:
-        if True:
-            x % 2 == 0 and y % 2 == 0
+        if x % 2 == 0 and y % 2 == 0:
             print(min(x, y))
-        elif False:
+        else:
             print(max(x, y))
-#the exception is meant to prevent raising an error in case of characters
-    except NameError:
-        print("Please enter numbers only.")
+    #the exception is meant to prevent raising an error in case of characters
+    except Exception as e:
+        print("Please enter numbers only.", e)
 
 compare(2, 6)
 compare(7, 9)
 compare(7, 7)
-compare(7, a)    #this raises a NameError so the exception does not work
+compare('d', 'a')
