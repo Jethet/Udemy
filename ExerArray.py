647c945218e2bd8fd65a0bf999c97e9091291d36
jethet <henriette.hettinga@gmail.com>
2018-12-01 16:28:52 +0100

    Wait

--- ExerArray.py
+++ ExerArray.py
@@ -2,7 +2,8 @@
 #  a 3 next to a 3 somewhere.
 
 def has_3(nums):
-    if '3,3' in nums or 33 in nums:
+    if 33 in nums or (3, 3) in nums:
         return True
 
-print(has_3([1,3,7,3,8]))
+print(has_3([1,33,7,3,8]))
+print(has_3([1,43, 35, 7]))
