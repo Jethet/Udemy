3e4c2564559829c9ad6deb0cca8df7ec0bbf0d4a
jethet <henriette.hettinga@gmail.com>
2018-12-01 16:50:40 +0100

    Final version eighth Function Practice exercise

--- ExerTriple.py
+++ ExerTriple.py
@@ -1,2 +1,9 @@
 #Given a string, return a string where for every character in the original
 # there are three characters
+
+def triple(letters):
+    triple = [x * 3 for x in letters]
+    return ''.join(triple)
+
+print(triple('paperdoll'))
+print(triple('codebar'))
