5735bd85c076cca894d9aa02165da011866403a7
jethet <henriette.hettinga@gmail.com>
2018-12-02 18:25:52 +0100

    Final code ninth Function Practice exercise

--- ExerBlack.py
+++ ExerBlack.py
@@ -5,13 +5,13 @@ If their sum exceeds 21 and there's an eleven, reduce the total sum by 10.
 Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
 """
 def blackjack(x, y, z):
-    if (x + y + z) <= 21:
-        return x + y + z
-    elif (x + y + z) > 21 and 11 in (x, y, z):
-        return x + y + z - 10
-    elif (x + y + z) - 10 > 21:
+    if sum((x, y, z)) <= 21:
+        return sum((x, y, z))
+    elif sum((x, y, z)) > 21 and 11 in (x, y, z):
+        return sum((x, y, z)) - 10
+    else:
         return 'BUST'
 
 print(blackjack(5, 7, 9))
-print(blackjack(15, 6, 18))
+print(blackjack(15, 6, 11))
 print(blackjack(9, 9, 9))
