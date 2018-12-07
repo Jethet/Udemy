9af0f1f24e54578af53655cd1ea96be076743d6f
jethet <henriette.hettinga@gmail.com>
2018-12-07 17:26:33 +0100

    Eleventh Function Practice exercise

--- ExerSpy.py
+++ ExerSpy.py
@@ -1,13 +1,18 @@
 #Write a function that takes in a list of integers and returns
 # True if it contains 007 in order
 def spy(values):
-    x = 0
-    y = 0
-    z = 7
-    for i in values:
-        if x and y and z in values:
-            return True
-        else:
-            return False
+    spylist = [0, 0, 7, 'x']
+#you iterate through all values to check if the value matches
+#  the value in position 0 in spylist. If it does match (because
+#  it is a 0 or 7) you delete that value from spylist
+    for num in values:
+        if num == spylist[0]:
+            spylist.pop(0)
+# if numbers are popped off spylist, 'x' is left and
+#   then the list has length of 1, len(spylist)== 1 is True if only 'x'
+#   is left, False if one or more of 0,0,7 still present (not popped).
+    return len(spylist) == 1
 
-print(spy([1, 0, 6, 0, 8, 7]))
+
+
+print(spy([7,2,0,3,4,0]))
