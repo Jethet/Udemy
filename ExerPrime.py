efec3b900fe5b9df32a4e5b3e36d73e1628b69d7
jethet <henriette.hettinga@gmail.com>
2018-12-07 17:55:56 +0100

    Draft eleventh Function Practice exercise

--- ExerPrime.py
+++ ExerPrime.py
@@ -4,3 +4,13 @@ exist up to and including a given number:
       count_primes(100) --> 25
 By convention, 0 and 1 are not prime
 """
+def prime(x):
+    if x >= 2:
+        for y in range(2, x):
+            if not (x % y):
+                return False
+    else:
+        return False
+    return True
+
+print(prime(14))
