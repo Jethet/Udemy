9f48c6e9b775a0f611e02c9cac46c3d85b2bf528
jethet <henriette.hettinga@gmail.com>
2018-12-07 15:25:42 +0100

    Draft tenth Function Practice exercise

--- ExerSixnine.py
+++ ExerSixnine.py
@@ -3,15 +3,14 @@
 #  Return 0 for no numbers.
 
 def six_nine(arr):
-#determine position of 6 and 9
     x = arr.index(6)
     y = arr.index(9)
-
 #check if there is a 6 (which means there is also a 9)
-    if arr.index(6) == 0:
-        return 0
-    else:
+    while True:
         result = sum(arr[0:x] + arr[(y+1):])
         return result
+        break
+    else:
+        return 0
 
-print(six_nine([1,3,5,6,7,9,11]))
+print(six_nine([1,3,5]))
