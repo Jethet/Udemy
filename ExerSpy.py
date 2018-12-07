85d0fbeea0a203ebd04f8209d8bba564e7cb3f96
jethet <henriette.hettinga@gmail.com>
2018-12-07 16:41:42 +0100

    Draft eleventh Function Practice exercise

--- ExerSpy.py
+++ ExerSpy.py
@@ -1,2 +1,13 @@
 #Write a function that takes in a list of integers and returns
 # True if it contains 007 in order
+def spy(values):
+    x = 0
+    y = 0
+    z = 7
+    for i in values:
+        if x and y and z in values:
+            return True
+        else:
+            return False
+
+print(spy([1, 0, 6, 0, 8, 7]))
