7a0aceaf2957152f718eb4dd320c2cac0b257fab
jethet <henriette.hettinga@gmail.com>
2018-12-07 16:21:41 +0100

    Tenth Function Practice exercise

--- ExerSixnine.py
+++ ExerSixnine.py
@@ -3,14 +3,28 @@
 #  Return 0 for no numbers.
 
 def six_nine(arr):
-    x = arr.index(6)
-    y = arr.index(9)
-#check if there is a 6 (which means there is also a 9)
-    while True:
-        result = sum(arr[0:x] + arr[(y+1):])
-        return result
-        break
-    else:
-        return 0
+#this function is meant to keep adding numbers except when a 6 appears
+    total = 0
+    add = True   #by default, keep adding: this triggers the loop
+#for each number in array while 'add' is True: do the following:
+    for num in arr:
+        while add:
+#the FIRST loop is triggered: if number not 6, add numbers to total
+            if num != 6:
+                total += num
+                break   #loop ends when addition is made
+#if number is 6, stop loop of adding; add = False, SECOND loop starts
+            else:
+                add = False
+#add is now False (or: not add:)
+        while not add:
+#no addition is made anymore until iteration gets to 9
+            if num != 9:
+                break
+#when the loop reaches nine, the FIRST loop starts again (add = True)
+            else:
+                add = True
 
-print(six_nine([1,3,5]))
+    return total
+
+print(six_nine([3,4,6,7,8,9,2,3,6,7,8,9,4,5]))
