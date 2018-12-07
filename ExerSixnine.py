2ffad3337282e8cce0bd6b533e3105ce1da3d1af
jethet <henriette.hettinga@gmail.com>
2018-12-07 15:11:52 +0100

    Tenth Function Practice exercise

--- ExerSixnine.py
+++ ExerSixnine.py
@@ -1,3 +1,17 @@
 #return the sum of numbers in array, except ignore sections starting
 #  with 6 and ending with 9 - only add numbers outside of that section.
 #  Return 0 for no numbers.
+
+def six_nine(arr):
+#determine position of 6 and 9
+    x = arr.index(6)
+    y = arr.index(9)
+
+#check if there is a 6 (which means there is also a 9)
+    if arr.index(6) == 0:
+        return 0
+    else:
+        result = sum(arr[0:x] + arr[(y+1):])
+        return result
+
+print(six_nine([1,3,5,6,7,9,11]))
