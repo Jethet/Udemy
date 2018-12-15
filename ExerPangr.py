009ed372998aa4d3d55ef921e7f6244f82a934b2
jethet <henriette.hettinga@gmail.com>
2018-12-15 17:06:33 +0100

    Seventh function and method test

--- ExerPangr.py
+++ ExerPangr.py
@@ -1,14 +1,19 @@
 #7-Write a function to check whether a string is a pangram or not
 import string
 
-def ispangram(strl, alphabet=string.ascii_lowercase):
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    alphaset = set(alphabet)
+    return alphaset <= set(str1.lower())
+
+print(ispangram('the quick brown fox jumps over the lazy dog'))
+print(ispangram('abcdefghijklmnopqrstuvwxyz'))
+print(ispangram('abcdefghijklmn'))
+"""
+def ispangram(str1, alphabet=string.ascii_lowercase):
     alphabet = alphabet.split()
-    strl = strl.split()
-    if alphabet in strl:
+    str1 = str1.split()
+    if alphabet in str1:
         return True
     else:
         return False
-
-print(ispangram('the quick brown fox jumped over the lazy dog'))
-print(ispangram('abcdefghijklmnopqrstuvwxyz'))
-print(ispangram('abcdefghijklmn'))
+        """
