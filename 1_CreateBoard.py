7afbf6cfd6d71b79395d31fbb6efc22402260f0d
jethet <henriette.hettinga@gmail.com>
2018-12-16 16:47:10 +0100

    Deleted from master

--- 1_CreateBoard.py
+++
@@ -1,6 +0,0 @@
-""" This function creates the board for tic-tac-toe: nine squares, 3 x 3
-A new board must be created each time a player answers 'yes' to the
-question if he wants to play.
-"""
-def create_board():
-    
--- 2_SquareChoice.py
+++
@@ -1,7 +0,0 @@
-def choice_board():
-    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    choice = int(input("Which square do you choose? "))
-    if choice in board:
-        print('need function')
-    else:
-        print("This is not a valid choice.")
