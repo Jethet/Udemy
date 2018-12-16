c907c7f707213bbc350aa9a5b441c3a75e0c3f12
jethet <henriette.hettinga@gmail.com>
2018-12-16 17:39:11 +0100

    Create player

--- 3_Player.py
+++ 3_Player.py
@@ -1,1 +1,14 @@
 #The game has two players and they have to choose if they play as X or O
+
+def player():
+    start = input("Would you like to play tic tac toe? Choose 1 for yes and 2 for no. ")
+    if start == '2':
+        print("Game over.")
+    else:
+        player = input("Choose which player you want to be: 1 for 'X' and 2 for '0'. ")
+        if player == '1':
+            print("Player X will start: you can choose first.")
+        else:
+            print("Player X always gets the first turn.")
+
+player()
