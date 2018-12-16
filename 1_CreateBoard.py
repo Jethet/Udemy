36939a652363c038e62440900b2a69fb1e25578e
jethet <henriette.hettinga@gmail.com>
2018-12-16 16:49:24 +0100

    Function to create board

--- 1_CreateBoard.py
+++ 1_CreateBoard.py
@@ -2,5 +2,10 @@
 A new board must be created each time a player answers 'yes' to the
 question if he wants to play.
 """
-def create_board():
-    
+def choice_board():
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    choice = int(input("Which square do you choose? "))
+    if choice in board:
+        print('need function')
+    else:
+        print("This is not a valid choice.")
