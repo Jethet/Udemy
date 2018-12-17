0db7d76f0bcc43d6c5dde047696c1c848bc123db
jethet <henriette.hettinga@gmail.com>
2018-12-17 15:47:58 +0100

    Use BeautifulTable

--- TestCode.py
+++ TestCode.py
@@ -1,21 +1,6 @@
 #board is [' 1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n', '7 |', ' 8 |', ' 9\n']
 """
-board = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n', '7 |', ' 8 |', ' 9\n']
-print(*board, sep='')
-
 board = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n',
         '7 |', ' 8 |', ' 9\n']
 print(*board, sep='')
 """
-def choice_board():
-    square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    choice = int(input("Which square do you choose? "))
-    if choice in square:
-        board = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n',
-                '7 |', ' 8 |', ' 9\n']
-        choice = choice - 1
-        board[choice] = 'X |'
-        print(*board, sep='')
-        return board
-
-choice_board()
