d06bcff15f6416503ede72ef5b75f948760daf27
jethet <henriette.hettinga@gmail.com>
2018-12-17 16:53:40 +0100

    Create board with BeautifulTable

--- TestCode.py
+++ TestCode.py
@@ -4,16 +4,12 @@ board = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n',
         '7 |', ' 8 |', ' 9\n']
 print(*board, sep='')
 """
-"""
-from beautifultable import BeautifulTable
-table = BeautifulTable()
-table.column_headers = ['name', 'rank', 'gender']
-table.append_row(['Jacob', '1', 'boy'])
-print(table)
-"""
 from beautifultable import BeautifulTable
 board = BeautifulTable()
 board.append_row(['1', '2', '3'])
 board.append_row(['4', '5', '6'])
 board.append_row(['7', '8', '9'])
 print(board)
+
+new_table = board[:3]
+print(new_table)
