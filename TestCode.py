d6420baddc6f57f1aa8ed922ab49ed00ddf32e56
jethet <henriette.hettinga@gmail.com>
2018-12-17 16:02:31 +0100

    Create 3 x 3 board with BeautifulTable

--- TestCode.py
+++ TestCode.py
@@ -4,3 +4,16 @@ board = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n',
         '7 |', ' 8 |', ' 9\n']
 print(*board, sep='')
 """
+"""
+from beautifultable import BeautifulTable
+table = BeautifulTable()
+table.column_headers = ['name', 'rank', 'gender']
+table.append_row(['Jacob', '1', 'boy'])
+print(table)
+"""
+from beautifultable import BeautifulTable
+board = BeautifulTable()
+board.append_row(['1', '2', '3'])
+board.append_row(['4', '5', '6'])
+board.append_row(['7', '8', '9'])
+print(board)
