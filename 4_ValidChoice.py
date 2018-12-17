7031fd12de91abf2e933ffb295a5f53ecfd98d73
jethet <henriette.hettinga@gmail.com>
2018-12-17 14:28:33 +0100

    Use dict for board

--- 4_ValidChoice.py
+++ 4_ValidChoice.py
@@ -5,14 +5,17 @@
 Player uses X or O
 """
 
-board = ['1 |', ' 2 |', ' 3\n', '4 |', ' 5 |', ' 6\n', '7 |', ' 8 |', ' 9\n']
+board = {'1':'1 |', '2':' 2 |', '3':' 3\n', '4':'4 |', '5':' 5 |',
+        '6':' 6\n', '7':'7 |', '8':' 8 |', '0':' 9\n'}
+print(str(board.values()))
+"""
 for choice in board:
     choice = int(input("What square do you choose? "))
     board[choice] = 'X'
     board.remove(choice)
     print(*board, sep='')
 
-
+"""
 """
 adict.update({key : value})  adds a key and value to dictionary
 adict[key] = value		also adds a key and value
