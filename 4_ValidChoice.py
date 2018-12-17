f16c6bc87eb2a0bf6c48836eec615dcb626cced9
jethet <henriette.hettinga@gmail.com>
2018-12-17 15:16:10 +0100

    Code for choice

--- 4_ValidChoice.py
+++ 4_ValidChoice.py
@@ -9,23 +9,13 @@ def choice_board():
     square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     choice = int(input("Which square do you choose? "))
     if choice in square:
-        square[choice] = 'X'
-        square.remove(choice)
-        print(*square, sep='')
-
-
+        board = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n',
+                '7 |', ' 8 |', ' 9\n']
+        choice = choice - 1
+        board[choice] = 'X |'
+        print(*board, sep='')
+        return board
     else:
         print("This is not a valid choice.")
 
-
-
-"""
-board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-choice = int(input("Which square do you choose? "))
-if choice in board:
-    if choice == 1:
-        print(' X | 2 | 3\n 4 | 5 | 6\n 7 | 8 | 9\n')
-
-board[n] = 'input'  = give position that gets new input
-board.remove(choice)
-"""
+choice_board()
--- TestCode.py
+++ TestCode.py
@@ -1,4 +1,21 @@
 #board is [' 1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n', '7 |', ' 8 |', ' 9\n']
+"""
+board = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n', '7 |', ' 8 |', ' 9\n']
+print(*board, sep='')
 
-square = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n', '7 |', ' 8 |', ' 9\n']
-print(*square, sep='')
+board = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n',
+        '7 |', ' 8 |', ' 9\n']
+print(*board, sep='')
+"""
+def choice_board():
+    square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    choice = int(input("Which square do you choose? "))
+    if choice in square:
+        board = ['1 |', ' 2 |', ' 3\n',  '4 |', ' 5 |', ' 6\n',
+                '7 |', ' 8 |', ' 9\n']
+        choice = choice - 1
+        board[choice] = 'X |'
+        print(*board, sep='')
+        return board
+
+choice_board()
