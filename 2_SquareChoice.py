db33b7a4e377249318bad06767939ea73f89e568
jethet <henriette.hettinga@gmail.com>
2018-12-17 14:34:10 +0100

    Change variable names for consistency

--- 2_SquareChoice.py
+++ 2_SquareChoice.py
@@ -1,9 +1,9 @@
 #This is the function that asks the player to choose where to tic
 
 def choice_board():
-    choice = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    square = int(input("Which square do you choose? "))
-    if square in choice:
+    square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    choice = int(input("Which square do you choose? "))
+    if choice in square:
         print('need function')
     else:
         print("This is not a valid choice.")
