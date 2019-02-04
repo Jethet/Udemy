d418329d10dd389ed844fe47caca8ba88f3960c1
jethet <henriette.hettinga@gmail.com>
2019-02-04 14:37:48 +0100

    Udemy OOP Challenge

---
+++ OOP Challenge.py
@@ -0,0 +1,17 @@
+"""
+Object Oriented Programming Challenge
+For this challenge, create a bank account class that has two attributes:
+    owner
+    balance
+and two methods:
+    deposit
+    withdraw
+
+As an added requirement, withdrawals may not exceed the available balance.
+Instantiate your class, make several deposits and withdrawals, and test to
+make sure the account can't be overdrawn.
+"""
+class Account:
+
+    def __init__(self,account1):
+        self.account1 = account1
