ed6d5bf8a91d972cc88c9e9f0d7ccc54b94351fe
jethet <henriette.hettinga@gmail.com>
2019-03-12 17:55:24 +0100

    Final Capstone Project 1: cost of tile to cover floor plan

--- CapstProj1.py
+++ CapstProj1.py
@@ -1,9 +1,12 @@
 # Find Cost of Tile to Cover W x H Floor - Calculate the total cost of tile
 # it would take to cover a floor plan of width and height, using a cost
 # entered by the user.
+import math
 
 def cost_tile(width,height,price):
-    w_tile = 30
-    h_tile = 30
-    floor = width * height
-    
+    w_tile = 0.30
+    h_tile = 0.30
+    tiles = (width*height) / 0.30
+    print('The total cost of the tiles is:', math.ceil(price*tiles))
+
+cost_tile(5,4,3.95)
