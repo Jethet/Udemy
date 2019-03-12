ebde5d9beda2e3f398f0e942c793780634518dbd
jethet <henriette.hettinga@gmail.com>
2019-03-12 17:22:42 +0100

    Final Capstone Project 1

---
+++ CapstProj1.py
@@ -0,0 +1,9 @@
+# Find Cost of Tile to Cover W x H Floor - Calculate the total cost of tile
+# it would take to cover a floor plan of width and height, using a cost
+# entered by the user.
+
+def cost_tile(width,height,price):
+    w_tile = 30
+    h_tile = 30
+    floor = width * height
+    
