cf80cbdc6357ab4d3515c377daeebd11c2568953
jethet <henriette.hettinga@gmail.com>
2019-03-13 10:11:41 +0100

    Correct mistake in code

--- CapstProj1.py
+++ CapstProj1.py
@@ -6,7 +6,7 @@ import math
 def cost_tile(width,height,price):
     w_tile = 0.30
     h_tile = 0.30
-    tiles = (width*height) / 0.30
+    tiles = (width / 0.30) * (height / 0.30)
     print('The total cost of the tiles is:', math.ceil(price*tiles))
 
 cost_tile(5,4,3.95)
