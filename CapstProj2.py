edaa52e87f83c9ec61914092a4746439b7a221b7
jethet <henriette.hettinga@gmail.com>
2019-04-09 15:40:27 +0200

    Change to pygame mixer

--- CapstProj2.py
+++ CapstProj2.py
@@ -2,7 +2,22 @@
 # A simple clock where it plays a sound after X number of minutes/seconds or
 # at a particular time.
 # mp3 file is called Digital Alarm Sound
+"""
+import playsound
+playsound.playsound('Digital_alarm_clock_sound.mp3')
 
+def alarm(time):
+    if time == 2:
+        print('\a')
+
+alarm(2)
+"""
+
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load('Digital_alarm_clock_sound.mp3')
+pygame.mixer.music.play()
 
 def alarm(time):
     if time == 2:
