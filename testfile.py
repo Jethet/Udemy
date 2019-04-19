95761ee0e25bd0d5a536f31d0925703b07cc80e4
jethet <henriette.hettinga@gmail.com>
2019-04-19 17:29:42 +0200

    Test regular expressions

--- testfile.py
+++ testfile.py
@@ -1,5 +1,5 @@
 import re
 
 test_text = "This library app allows you to add a book, to search for an author or title,"
-pattern = ('add')
+pattern = ['pa*', 'pa+']
 print(re.search(pattern, test_text))
