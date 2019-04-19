228807d47e4a982c2d61138a8929f37cc905c49d
jethet <henriette.hettinga@gmail.com>
2019-04-19 17:39:46 +0200

    Regex testing

--- testfile.py
+++ testfile.py
@@ -2,8 +2,10 @@ import re
 
 test_text = "This library app allows you to add a book, to search for an author or title,"
 patterns = ('pa*', 'pa+', 'aa?', 'da?')
+pattern2 = r'\s+'
 
 for pattern in patterns:
     print(re.findall(pattern,test_text))
 
 print(re.findall('[^,]+', test_text))
+print(re.findall(pattern2, test_text))
